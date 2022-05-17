50f88f224f09af5b73bb5e92ca79fd58396688f4
cheoklate <gcheok88@gmail.com>
2022-05-18 02:15:42 +0800

    router template

--- src/index.js
+++ src/index.js
@@ -1,17 +1,47 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import React, { lazy, Suspense } from 'react';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter, Routes, Route, Switch } from 'react-router-dom';
+import './styles.scss';
+import Backdrop from '@mui/material/Backdrop';
+import CircularProgress from '@mui/material/CircularProgress';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+
+const Homepage = lazy(() => import('./components/Blog/Blog.jsx'));
+const SignUpPage = lazy(() => import('./components/SignUp.jsx'));
+const SignInPage = lazy(() => import('./components/SignIn.jsx'));
+const Dashboard = lazy(() => import('./components/Dashboard/Dashboard.jsx'));
+
+const rootElement = document.createElement('div');
+
+document.body.appendChild(rootElement);
+
+const root = createRoot(rootElement);
+
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+	<BrowserRouter>
+		<Suspense
+			fallback={
+				<div>
+					<Backdrop
+						sx={{ color: '#fff', zIndex: (theme) => theme.zIndex.drawer + 1 }}
+						open={open}
+					>
+						<CircularProgress color='inherit' />
+					</Backdrop>
+				</div>
+			}
+		>
+			<Routes>
+				<Route path='/' element={<Homepage />} />
+				<Route path='signup' element={<SignUpPage />} />
+				<Route path='signin' element={<SignInPage />} />
+				<Route path='dashboard' element={<Dashboard />} />
+			</Routes>
+		</Suspense>
+	</BrowserRouter>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
