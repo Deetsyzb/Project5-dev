b5577b1884fd7d62433e0c50b92bc3b6f05fdc37
cheoklate <gcheok88@gmail.com>
2022-05-18 01:17:27 +0800

    added Controllers and Models files

---
+++ models/Users.ts
@@ -0,0 +1,32 @@
+const { Schema, model } = require("mongoose");
+
+const userSchema = new Schema(
+  {
+    name: String,
+    email: {
+      type: String,
+      lowercase: true,
+      required: true,
+      unique: true,
+      trim: true,
+      match: /.+\@.+\..+/,
+    },
+    password: { type: String, required: true },
+    address: { type: String, required: true, unique: true },
+    // walletBalance: Number,
+    // requests: []
+    friends: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+  },
+  /**
+   * The { timestamps: true } tells Mongoose to automatically add createdAt and updatedAt properties to the schema. By default, createdAt and updatedAt are of type "Date".
+   * When you update a document, Mongoose automatically increments updatedAt.
+   * */
+  { timestamps: true }
+);
+
+module.exports = model("User", userSchema);
