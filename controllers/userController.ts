b06620498c53b843931dad647831e91d42737701
cheoklate <gcheok88@gmail.com>
2022-05-20 02:07:19 +0800

    baseController and userController templates

---
+++ controllers/userController.ts
@@ -0,0 +1,39 @@
+import BaseController from './baseController.ts';
+
+class UserController extends BaseController {
+	/** Returns a token and the userId to the FE if log in is successful
+	 * @param {string} email
+	 * @param {string} password
+	 */
+	async logIn(req, res) {
+		console.log('logging in');
+		const { email, password } = req.body;
+		console.log('email', email);
+		const user = await this.model.findOne({ email });
+		try {
+			if (!user) {
+				res.send('The email or password is incorrect');
+			} else {
+				// salts and hashes the first password,
+				// then compare with the hashed password from the db to make sure that they are the same.
+				// If they are, return true, else return false.
+				const logInSuccess = await bcrypt.compare(password, user.password);
+
+				if (!logInSuccess) {
+					res.send('The email or password is incorrect');
+					return;
+				}
+				const payload = {
+					_id: user.id,
+					name: user.name,
+					address: user.address,
+				};
+				const token = jwt.sign(payload, this.salt, { expiresIn: '6h' });
+				console.log(user);
+				res.send({ token, userId: user.id, success: true });
+			}
+		} catch (err) {
+			this.errorHandler(err, res);
+		}
+	}
+}
