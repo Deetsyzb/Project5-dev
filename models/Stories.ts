57eb946985b4c0f724fbf6395ebc830a726df94e
Deetzyzb <d.shawnyeo@gmail.com>
2022-06-01 20:36:15 +0800

    Story Save Routes

--- models/Stories.ts
+++ models/Stories.ts
@@ -3,6 +3,7 @@ import { Schema, model } from 'mongoose';
 const storiesSchema: Schema = new Schema(
 	{
 		creatorID: Number,
+		title: String,
 		content: String,
 	},
 
--- routes/userRoutes.ts
+++ routes/userRoutes.ts
@@ -1,10 +1,12 @@
 import { Router } from 'express';
 import UserController from '../controllers/userController';
+import StoryController from '../controllers/storyController';
 
 const router: Router = Router();
 
 // router.post('/hello', UserController.helloworld)
 router.post('/login', UserController.login);
 
+router.post('/save', StoryController.save)
 
 export default router;
